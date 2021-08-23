ac3076922d889ad11fa1e0f07c6cee542d194c29
kamilkow1123 <22kamil.kow@gmail.com>
2021-08-23 21:38:24 +0200

    Styling navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,32 +1,45 @@
 import { Link } from "react-router-dom";
 //icons
-import { FaHeart, FaShoppingCart, FaUser, FaAngleDown } from "react-icons/fa";
+import {
+    FaHeart,
+    FaShoppingCart,
+    FaUser,
+    FaAngleDown,
+    FaBars,
+} from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <nav className="nav">
             <div className="nav__wrapper">
-                <Link to="/" className="nav__logo">
-                    LOGO
-                </Link>
-                <ul className="nav__menu">
-                    <li className="nav__item">Categories</li>
-                    <li className="nav__item">Payment</li>
-                    <li className="nav__item">Warranty</li>
-                    <li className="nav__item">Credit</li>
-                </ul>
-                <div className="nav__item">
-                    Catalog <FaAngleDown />
+                <div className="nav__group">
+                    <Link to="/" className="nav__logo">
+                        LOGO
+                    </Link>
+                    <ul className="nav__menu">
+                        <li className="nav__item">Categories</li>
+                        <li className="nav__item">Payment</li>
+                        <li className="nav__item">Warranty</li>
+                        <li className="nav__item">Credit</li>
+                    </ul>
                 </div>
-                <div className="nav__icons">
-                    <div className="nav__icon">
-                        <FaHeart />
+                <div className="nav__group">
+                    <div className="nav__dropdown">
+                        Catalog <FaAngleDown />
                     </div>
-                    <div className="nav__icon">
-                        <FaShoppingCart />
+                    <div className="nav__icons">
+                        <div className="nav__icon">
+                            <FaHeart />
+                        </div>
+                        <div className="nav__icon">
+                            <FaShoppingCart />
+                        </div>
+                        <div className="nav__icon">
+                            <FaUser />
+                        </div>
                     </div>
-                    <div className="nav__icon">
-                        <FaUser />
+                    <div className="nav__hamburger">
+                        <FaBars />
                     </div>
                 </div>
             </div>
